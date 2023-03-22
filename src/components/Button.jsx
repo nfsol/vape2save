fd66cb1f26af7a223a392743b9068abf3f79f9f7
nfsol <tbpeddle@gmail.com>
2023-03-22 15:50:07 -0230

    incremental work learning styled components, basic welcome page coming together

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,52 +2,35 @@ import React from "react";
 import styled from "styled-components";
 const BaseButton = styled.button`
   appearance: none;
-  background-color: #000000;
+  background-color: ${(props) => props.type === "primary" ? "white" : "black"};
   border: 2px solid #1a1a1a;
   border-radius: 15px;
   box-sizing: border-box;
-  color: #ffffff;
+  color: ${(props) => (props.type === "primary" ? "black" : "white")};
   cursor: pointer;
   display: inline-block;
-  font-family: Roobert, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica,
-    Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
   font-size: 16px;
-  font-weight: 600;
-  line-height: normal;
-  margin: 0;
+  font-weight: 500;
   min-height: 60px;
-  min-width: 0;
-  outline: none;
-  padding: 16px 24px;
-  text-align: center;
-  text-decoration: none;
+  max-height: 180px;
+  max-width: 300px;
+  padding: 16px 0px;
   transition: all 300ms cubic-bezier(0.23, 1, 0.32, 1);
   user-select: none;
   -webkit-user-select: none;
   touch-action: manipulation;
   width: 100%;
   will-change: transform;
-
-  &:disabled {
-    pointer-events: none;
-  }
-  ,
   &:hover {
     box-shadow: rgba(0, 0, 0, 0.25) 0 8px 15px;
     transform: translateY(-2px);
-  }
-
-  ,
+  },
   &:active {
     box-shadow: none;
     transform: translateY(0);
   }
 `;
 
-
 export const Button = (props) => {
-  return (
-    <BaseButton className={props.type}>{props.content}</BaseButton>
-  )
-}
-
+  return <BaseButton type={props.type}>{props.content}</BaseButton>;
+};
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,27 +1,50 @@
 import React from "react";
-import smoking from '../assets/smoking.jpg'
+import smoking from "../assets/smoking.jpg";
 import { Button } from "./Button";
+import styled from "styled-components";
+const OverlaidImage = styled.div`
+  width: 100%;
+  height: 100%;
+  margin-bottom: 20px;
+  background-image: url(${smoking});
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  display: flex;
+  align-items: space-between;
+`;
+
+const ImageOverlay = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+const ButtonContainer = styled.div`
+  align-self: center;
+`;
+
 export const Welcome = () => {
   return (
     <>
-    <div className="container">
-      <img
-      className="pic"
-      src={smoking}
-      alt="One standing lit cigarette among others snuffed out"
-      />
-      <div className="welcomeText">
-        Still smoking? Yikes. I know you've heard all the anxiety inducing
-        health arguments before, and obviously that hasn't worked so lets try
-        something else. Do you like... money?
-      </div>
-    </div>
       <div className="container">
-      <Button type="primary" content="Uh...Yes?"/>
-      <button>No, I seek to rid myself of all worldy possessions.</button>
+        <OverlaidImage>
+          <ImageOverlay>
+            Still smoking? Yikes. I know you've heard all the anxiety inducing
+            health arguments before, and obviously that hasn't worked. Lets try
+            something else. Do you like... saving money?
+            <ButtonContainer>
+              <Button type="primary" content="Uh...Yes?" />
+              <Button
+                type="secondary"
+                content="No! Possessions are a distraction!"
+              />
+            </ButtonContainer>
+          </ImageOverlay>
+        </OverlaidImage>
       </div>
-      </>
+    </>
   );
 };
-
-
